71b5207fd56a7a331b1ad88bd35a689a5f57dd1e
Ravi Sudha <ravisudha.610weblab@gmail.com>
2022-07-15 12:41:21 +0530

    removed the react-assesible-accordian and create the faqs functionality with custom code instead of node package

--- src/Components/Faqs/Faqs.js
+++ src/Components/Faqs/Faqs.js
@@ -2,20 +2,17 @@ import React, {useState} from 'react';
 import './Faqs.scss';
 import {Container, Row, Col} from 'react-bootstrap';
 import { FaqsData } from '../Data/FaqsData';
-import {Accordion, AccordionItem, AccordionItemHeading, AccordionItemButton, AccordionItemPanel} from 'react-accessible-accordion';
-import 'react-accessible-accordion/dist/fancy-example.css';
-
 
 function Faqs() {
 
-    const [isActive, setActive] = useState(false);
 
-    function activeTab(e) {
-        setActive(!isActive);
+
+    const handleClick = event => {
+        event.currentTarget.classList.toggle('active');
     }
 
     /* Load More Function */
-    const faqsPerRow = 4;
+    const faqsPerRow = 3;
     const [next, setNext] = useState(faqsPerRow);
 
     const loadFaqs = () => {
@@ -32,38 +29,22 @@ function Faqs() {
                             <h1 className='title'>Faqs</h1>
                             <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
                             <div className='faqs_content'>
-                                <Accordion allowMultipleExpanded allowZeroExpanded>
-                                    {FaqsData.slice(0, next).map(function(value, index){
-                                        return(
-                                            <AccordionItem key={index} className='faq_item'>
-                                                <AccordionItemHeading className='faq_title'>
-                                                    <AccordionItemButton>{value.title}</AccordionItemButton>
-                                                </AccordionItemHeading>
-                                                <AccordionItemPanel className='faq_desc'>
-                                                    <p>{value.description}</p>
-                                                </AccordionItemPanel>
-                                            </AccordionItem>
-                                        );
-                                    })}
-                                </Accordion>
-                                {next < FaqsData?.length && (
-                                    <div className='global_btn'>
-                                        <a className="btn_text" onClick={loadFaqs}>Load More</a>
-                                    </div>
-                                )}
-                        
-                            </div>
-                            {/* <div className='faqs_content'>
-                                {FaqsData.map(function(value){
+                                {FaqsData.slice(0, next).map(function(value){
                                     const {id, title, description} = value;
                                     return(
-                                        <div key={id} className={`faq_item ${isActive ? 'active' : ''}`} onClick={ activeTab }>
+                                        <div key={id} className="faq_item" onClick={handleClick} >
                                             <h2 className='faq_title'>{title}</h2>
                                             <p className='faq_desc'>{description}</p>
                                         </div>
                                     );
                                 })}
-                            </div> */}
+
+                                {next < FaqsData?.length && (
+                                    <div className='global_btn'>
+                                        <a className="btn_text" onClick={loadFaqs}>Load More</a>
+                                    </div>
+                                )}
+                            </div>
                         </Col>
                     </Row>
                 </Container>
